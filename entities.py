fde4e3defa7867f3691e1b43a3f9f1ea348d35ea
Revellyon <martincondegrande@hotmail.com>
2020-01-31 03:35:58 +0100

    Raqueta en movimiento con limites pantalla

--- entities.py
+++ entities.py
@@ -5,6 +5,7 @@ FPS = 60
 
 class Racket(pg.sprite.Sprite):
     pictures = 'racket_horizontal.png'
+    speed = 10
 
     def __init__(self, x=355, y=580):
         self.x = x
@@ -13,9 +14,19 @@ class Racket(pg.sprite.Sprite):
         pg.sprite.Sprite.__init__(self)
 
         self.image = pg.image.load('resources/{}'.format(self.pictures)).convert_alpha()
+
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.w = self.rect.w
+        self.h = self.rect.h
         
-        self.otraimage = pg.Surface ((90,10), SRCALPHA, 32)
-        self.otraimage.fill((89,89,89))
+    def go_left(self):
+        self.rect.x = max(0, self.rect.x - self.speed) #haces la resta y de entre el resultado y 0, me coges el mayor
+        if self.x <= 0:
+            self.rect.x = 0
+
+    def go_right(self):
+        self.rect.x = min(self.rect.x + self.speed, 800-self.w)  #haces la resta y de entre el resultado y 800, me coges el menor
+        if self.x >= 800:
+            self.rect.x = 800
