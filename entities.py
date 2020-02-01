80095eab4b3c5a97a1e73ff539788caab3c3d1d3
Revellyon <martincondegrande@hotmail.com>
2020-02-01 03:06:00 +0100

    Control raqueta y bola + numero de vidas

--- entities.py
+++ entities.py
@@ -1,11 +1,13 @@
 import pygame as pg
 from pygame.locals import *
+from random import choice
 
 FPS = 60
 
 class Racket(pg.sprite.Sprite):
     pictures = 'racket_horizontal.png'
     speed = 10
+    lives = 3
 
     def __init__(self, x=355, y=580):
         self.x = x
@@ -42,24 +44,38 @@ class Ball(pg.sprite.Sprite):
         self.image = pg.image.load('resources/{}'.format(self.pictures)).convert_alpha()
     
         self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
         self.w = self.rect.w
         self.h = self.rect.h
+
+        self.start()
+
+    def start(self):
+        self.rect.x = self.x
+        self.rect.y = self.y 
+        self.speed = 5
+        self.dy = 1
+        self.dx = choice([-1, 1])
 
     def update(self, dt):
         self.rect.x = self.rect.x + self.speed * self.dx #la imgagen se mueve +velocidad por variacion derecha
         self.rect.y = self.rect.y + self.speed * self.dy #la imgagen se mueve +velocidad por variacion abajo
 
-        if self.rect.y >=600: #si es mayor de 600
-            self.dy = self.dy * -1 #inviertes la direccion y ahora seguirá hacia la derecha pero arriba, al haber llegado al limite, pierde vida
-        
-        if self.rect.y <=0: #si es mayor de 0
+        if self.rect.y >=600 - self.h: #si es mayor de 600
+            #self.dy = self.dy * -1 #inviertes la direccion y ahora seguirá hacia la derecha pero arriba, al haber llegado al limite, pierde vida
+            self.speed = 0
+
+        if self.rect.y <=0: #si es menor de 0
             self.dy = self.dy * -1
 
-        if self.rect.x <=0: #si es mayor de 0
+        if self.rect.x <=0: #si es menor de 0
             self.dx = self.dx * -1
         
-        if self.rect.x >=800: #si es mayor de 600
+        if self.rect.x >=775: #si es mayor de 600
             self.dx = self.dx * -1
+
+    def test_collision(self, group):
+        candidates = pg.sprite.spritecollide(self, group, False) #esto está en la documentacion pygame
+        if len(candidates) > 0:
+            self.dy *= -1
+
 
